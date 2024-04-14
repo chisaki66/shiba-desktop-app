47c99d48494cec5ccd4b7bca2e251935f859e972
chisaki66 <witness6961@gmail.com>
2024-04-14 23:23:49 +0900

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,11 @@
 import './App.scss';
-import { useState } from 'react';
+import ListGroup from './components/ListGroup';
 
 const App = () => {
-  const [task, setTask] = useState('');
-  const [todos, setTodos] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (task === '') return;
-    setTodos((todos) => [...todos, { task, isCompleted: false }]);
-    setTask('');
-  };
-
-  const handleUpdateTask = (index) => {
-    const newTodos = todos.map((todo, todoIndex) => {
-      if (todoIndex === index) {
-        todo.isCompleted = !todo.isCompleted;
-      }
-      return todo;
-    });
-    setTodos(newTodos);
-  };
-
-  const handleEditTask = (index, newName) => {
-    const updatedTasks = [...todos];
-    updatedTasks[index].task = newName;
-    setTodos(updatedTasks);
-  };
-
-  const handleRemoveTask = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
   return (
     <div>
       <header className="header"></header>
-      <main className="main">
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            className="form__input"
-            value={task}
-            placeholder="タスクを入力..."
-            onChange={(event) => setTask(event.target.value)}
-          />
-        </form>
-        <ul className="list">
-          {todos.map((todo, index) => (
-            <li className="list__item" key={index}>
-              <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={() => handleUpdateTask(index)}
-              />
-              {todo.isCompleted ? (
-                <span className="list__done-list-item">{todo.task}</span>
-              ) : (
-                <input
-                  className="list__input-list-item"
-                  value={todo.task}
-                  onChange={(event) => handleEditTask(index, event.target.value)}
-                />
-              )}
-              <div
-                className="list__action-button"
-                onClick={() => handleRemoveTask(index)}
-                style={{ cursor: 'pointer' }}
-              >
-                X
-              </div>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <ListGroup />
       <footer></footer>
     </div>
   );
